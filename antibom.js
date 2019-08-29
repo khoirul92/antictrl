f1cbe140d4fbee0981349ed2e288961576cbc60f
khoirul92 <54443082+khoirul92@users.noreply.github.com>
2019-08-29 22:16:24 +0700

    Create antibom.js

---
+++ antibom.js
@@ -0,0 +1,4 @@
+<script async='true' type='text/javascript'> //<![CDATA[ function downloadJSAtOnload()
+{var e=document.createElement("script");e.src="https://cdn.statically.io/gh/randiid/randidotid/fdb8820c/antiklik.js",document.body.appendChild(e)}
+window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent
+("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload; randidotid.init({click:2,interval:23000}); //]]> </script>
