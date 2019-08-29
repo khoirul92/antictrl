a18385fa0d1577540caa0ae81d493dcb5933e3f3
khoirul92 <54443082+khoirul92@users.noreply.github.com>
2019-08-29 20:38:56 +0700

    Update antibeck.js

--- antibeck.js
+++ antibeck.js
@@ -1,2 +1,2 @@
 history.pushState(null,null,'#');window.addEventListener('popstate',function(event)
-{history.pushState(null,null,'#');});
+{history.pushState(null,null,'location.href="https://klikjenar.blogspot.com/search/label/Tips"');});
